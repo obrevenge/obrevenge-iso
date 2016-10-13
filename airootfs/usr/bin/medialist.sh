d10c61150fffe68807ce0a8baa03827ced53d9df
ajjames31 <ajjames31@gmail.com>
2016-10-13 09:52:47 -0500

    updated source for 2016.10.12 build

--- airootfs/usr/bin/medialist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# OBRevenge Media list tool
-
-# This is a tool to make a list of media applications to install 
-
-
-    zenity --list --text "Select the Media Applications that You Would Like to Install" --checklist --column "Select" --column "Applications" TRUE "gimp " TRUE "vlc " FALSE "parole " TRUE "audacious " FALSE "banshee " FALSE "clementine " FALSE "gthumb " FALSE "shotwell " FALSE "brasero " FALSE "audacity " FALSE "simplescreenrecorder " FALSE "xfburn " FALSE "kdenlive " FALSE "obs-studio " --height 400 > /etc/obrevenge/.tmp/med.txt
-    sed -i -e 's/[|]//g' /etc/obrevenge/.tmp/med.txt
--- airootfs/usr/bin/officelist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# OBRevenge Office list tool
-
-# This is a tool to make a list of office applications to install 
-
-    zenity --list --text "Select the Office Applications that You Would Like to Install" --checklist --column "Select" --column "Applications" TRUE "libreoffice-fresh " FALSE "abiword " FALSE "gnumeric " FALSE "pdfmod " TRUE "evince " --height 400 > /etc/obrevenge/.tmp/off.txt
-    sed -i -e 's/[|]//g' /etc/obrevenge/.tmp/off.txt
